89cd150a5179b1087e5378b43cd53910c4a08247
RobertLitzke <robert.litzke@gmail.com>
2024-06-01 18:02:24 +0100

    Add documentation, format python, fix bugs

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# pcb-bom
-Utility for tabulating and bulk-ordering electronic components
+# PCB Bill of Materials (pcb-bom)
+Utility for tabulating and bulk-ordering electronic components from poorly-structured lists of data (e.g. PedalPCB component lists). It was created for guitar pedal component calculations.
+
+## What does it do?
+
+You have list of random electronic parts you want to order. It may be poorly structured, not have columns, and it may have many repeated elements. Perhaps it's a list for many projects at once, increasing the risk of duplicate entries.
+
+You do the following:
+* Put any components into a raw text file, according to the format below.
+* Run `python sort_components.py -in_file:<text file name>`
+* You receive the following files:
+  * *Parts list* - The parts list, with delimiters between columns, suitable for pasting into a spreadsheet.
+  * *Totals* - The total number of components of each type, across all projects.
+  * *Projects* - A list of just the project names, in case you want to store them somewhere.
+
+## What format does it support?
+
+In short this was designed to take parts lists from PedalPCB. You can see an overall example of a working input in `examples/input.txt`. A list of the following format is currently supported:
+
+### Electrical components
+`<Component name in project> <Component specification> <Optional Description>`
+
+* *Component name in project* - This would be things like `R1` or `IC2`
+* *Component specification* - This would be things like `4K7` or `100u`
+* *Optional description* - Any additional text
+
+Each part must be on its own line. Here are some examples:
+
+```
+R14 100K
+C100 10u
+D5 1N4148
+LEVEL B100K
+```
+
+### Other information
+
+You can also specify projects:
+
+`*<Project name> <URL>`
+
+The project row must start with an asterisk and must include a URL at the end. The URL can be anything. Here are some examples:
+
+```
+*Greengage https://docs.pedalpcb.com/project/Greengage.pdf
+*Shika https://docs.pedalpcb.com/project/Shika-PedalPCB.pdf
+```
+
+Anything else (like part-type headers, such as `Resistor`) can be included on their own row. In the worst case they just will be unparseable and noted as such.
+
+## What are the outputs?
+
+Running the script will output 3 files:
+
+* `Totals`: A list of the total components, in the format `<Component type>|<Component specification>|<Count>`. This file will also output the total count of components which had an unsupported format and which could not be interpreted. Check this carefully to avoid missing parts.
+* `Parts`: A structured list that corresponds to each original input row. The program will attempt to add delimiters to improve the structure of this output.
+* `Projects`: A list of each individual project, can be used as a handy reference.
+
+### What do I do with them?
+
+You can just keep the files or look at the totals, but they are designed to be pasted into a spreadsheet, such as Google Sheets. Create a new sheet, copy the content of the file you want in a spreadsheet, and paste it into the new sheet. A small popup will appear, and you should select `Paste formatting` -> `Split text to columns` -> `Separator: custom` -> `Type "|"`. You will now have your data in a spreadsheet with columns you can easily tabulate, etc. The `pcb-bom` allows you to choose a column delimiter with the `separator` command-line argument. The default is the pipe character (`|`), since it is rarely used (unlike commas, spaces, or semicolons).
--- sort_components.py
+++ sort_components.py
@@ -1,106 +1,217 @@
-import re
-import sys
+import argparse, re, sys
 
-resistors_re = re.compile(r'^(R\d+|RLED)?\s(\d+[KMR]\d?)\s?(.*)?')
-resistor_value_re = re.compile(r'\D*\d*\s?(\d+[KMR]\d?).*')
+resistors_re = re.compile(r"^(R\d+|RLED)?\s(\d+[KMR]\d*)\s?(.*)?")
+resistor_value_re = re.compile(r"\D*\d*\s?(\d+[KMR]\d*).*")
 
-capacitors_re = re.compile(r'^(C\d+)?\s(\d+[uUnNpP])\s?(.*)?')
-capacitor_value_re = re.compile(r'\D*\d*\s?(\d+[uUnNpP])\s?(.*)?')
+capacitors_re = re.compile(r"^(C\d+)?\s(\d+[uUnNpP])\s?(.*)?")
+capacitor_value_re = re.compile(r"\D*\d*\s?(\d+[uUnNpP])\s?(.*)?")
 
-diodes_re = re.compile(r'^(D\d+)?\s(\d*[1N|V]\d*|\dmm Red(?: LED)?|GE|BAT\d*)\s?(.*)?')
-diode_value_re = re.compile(r'\D*\d*\s?(\d*[1N|V]\d*|\dmm Red(?: LED)?|GE|BAT\d*).*')
+diodes_re = re.compile(
+    r"^(D\d+)?\s(\d*[1N|V]\d*\w?|\dmm Red(?: LED)?|GE|BAT\d*)\s?(.*)?"
+)
+diode_value_re = re.compile(r"\D*\d*\s?(\d*[1N|V]\d*\w?|\dmm Red(?: LED)?|GE|BAT\d*).*")
 
-ics_re = re.compile(r'^(IC\d+)?\s(\d*(?:RC|CD|AD|[TLNV])\d*\w*)\s?(.*)?')
-ic_value_re = re.compile(r'\D*\d*\s?(\d*(?:RC|CD|AD|[TLNV])\d*\w*).*')
+ics_re = re.compile(r"^(IC\d+)?\s(\d*(?:RC|CD|AD|[TLNV])\d*\w*)\s?(.*)?")
+ic_value_re = re.compile(r"\D*\d*\s?(\d*(?:RC|CD|AD|[TLNV])\d*\w*).*")
 
-pots_re = re.compile(r'^(GAIN|TONE|LEVEL|RATE|DEPTH|LAG|BLEND|TRIM|VOLUME|SUSTAIN|HARMONICS|BALANCE|OCTAVE|FUZZ|VOICE|DRIVE|PRES|RANGE|BOOST|HEAT|BIAS|DIRT|MORE)?\s([ABWC]?\d+[MK])\s?(.*)?')
-pot_value_re = re.compile(r'\D*\d*\s([ABCW]?\d+[MK]).*')
+pots_re = re.compile(
+    r"^(GAIN|TONE|LEVEL|RATE|DEPTH|LAG|BLEND|TRIM|VOLUME|SUSTAIN|HARMONICS|BALANCE|OCTAVE|FUZZ|VOICE|DRIVE|PRES|RANGE|BOOST|HEAT|BIAS|DIRT|MORE)?\s([ABWC]?\d+[MK])\s?(.*)?"
+)
+pot_value_re = re.compile(r"\D*\d*\s([ABCW]?\d+[MK]).*")
 
-switches_re = re.compile(r'^(SW\d|MODE|SHAPE)?\s((?:[ONF/]+\s?Toggle switch, )?SPDT\s?(?:2-position)?\s?\([ONF/]+\))\s?(.*)?')
-switch_value_re = re.compile(r'\D*\d*\s?((?:[ONF/]+\s?Toggle switch, )?SPDT\s?(?:2-position)?\s?\([ONF/]+\)).*')
+switches_re = re.compile(
+    r"^(SW\d|MODE|SHAPE)?\s((?:[ONF/]+\s?Toggle switch, )?SPDT\s?(?:2-position)?\s?\([ONF/]+\))\s?(.*)?"
+)
+switch_value_re = re.compile(
+    r"\D*\d*\s?((?:[ONF/]+\s?Toggle switch, )?SPDT\s?(?:2-position)?\s?\([ONF/]+\)).*"
+)
 
-transistors_re = re.compile(r'^(Q\d+)?\s((?:MPF|NPN|BC)\d*\*?|\d*[2N]\d*A?|\d)\s?(.*)?')
-transistor_value_re = re.compile(r'\D*\d*\s?((?:MPF|NPN|BC)\d*\*?|\d*[2N]\d*A?|\d).*')
+transistors_re = re.compile(r"^(Q\d+)?\s((?:MPF|NPN|BC)\d*\*?|\d*[2N]\d*A?|\d)\s?(.*)?")
+transistor_value_re = re.compile(r"\D*\d*\s?((?:MPF|NPN|BC)\d*\*?|\d*[2N]\d*A?|\d).*")
 
-pedal_re = re.compile(r'^\*([\s\w\d]+)\s(http).*')
-part_type_re = re.compile(r'.*(RESISTORS\s?(?:\(1/4W\))?|CAPACITORS|TRANSISTORS|DIODES|SWITCHES|POTS|POTENTIOMETERS|INTEGRATED CIRCUITS|SEMICONDUCTORS).*', re.IGNORECASE)
+project_re = re.compile(r"^\*([\s\w\d]+)\s(http.*)")
+part_type_re = re.compile(
+    r".*(RESISTORS\s?(?:\(1/4W\))?|CAPACITORS|TRANSISTORS|DIODES|SWITCHES|POTS|POTENTIOMETERS|INTEGRATED CIRCUITS|SEMICONDUCTORS).*",
+    re.IGNORECASE,
+)
 
-def process_part_type(line, out, value_prefix):
-	out += "\n" + value_prefix + part_type_re.match(line).groups()[0]
-	return out
 
-def process_pedal_name(line, out, value_prefix):
-	out += "\n" + value_prefix + pedal_re.match(line).groups()[0]
-	return out
+def process_part_type(line, out, value_prefix, separator):
+    out += f"\n{value_prefix}{separator}{part_type_re.match(line).groups()[0]}"
+    return out
 
-def process_simple(line, out, totals, line_regex, value_regex, value_prefix):
-	line_regex_result = line_regex.match(line)
-	out += f"\n{'|'.join(line_regex_result.groups())}"
-	value_regex_result = value_prefix + value_regex.match(line).groups()[0]
-	if value_regex_result in totals:
-		totals[value_regex_result] += 1
-	else:
-		totals[value_regex_result] = 1
-	return [out, totals]
 
-def process(str_in, out, totals, pedals):
-	lines = str_in.split('\n')
-	for line in lines:
-		#print(line)
-		pedal_match = pedal_re.match(line)
-		if pedal_match:
-			out = process_pedal_name(line, out, 'PEDAL: ')
-			pedals.append(pedal_match.groups()[0])
-			continue
-		part_type_match = part_type_re.match(line)
-		if part_type_match:
-			out = process_part_type(line, out, 'PART CLASS: ')
-			continue
-		resistor_match = resistors_re.match(line)
-		capacitor_match = capacitors_re.match(line)
-		ic_match = ics_re.match(line)
-		diodes_match = diodes_re.match(line)
-		transistors_match = transistors_re.match(line)
-		pots_match = pots_re.match(line)
-		switches_match = switches_re.match(line)
+def process_project_name(line, out, value_prefix, separator):
+    out += f"\n{value_prefix}{separator}{project_re.match(line).groups()[0]}{separator}{project_re.match(line).groups()[1]}"
+    return out
 
-		if ic_match:
-			[out, totals] = process_simple(line, out, totals, ics_re, ic_value_re, 'IC|')
-		elif diodes_match:
-			[out, totals] = process_simple(line, out, totals, diodes_re, diode_value_re, 'DIODE|')
-		elif pots_match:
-			[out, totals] = process_simple(line, out, totals, pots_re, pot_value_re, 'POT|')
-		elif transistors_match:
-			[out, totals] = process_simple(line, out, totals, transistors_re, transistor_value_re, 'TRANSISTOR|')
-		elif switches_match:
-			[out, totals] = process_simple(line, out, totals, switches_re, switch_value_re, 'SWITCH|')
-		elif resistor_match:
-			[out, totals] = process_simple(line, out, totals, resistors_re, resistor_value_re, 'RESISTOR|')
-		elif capacitor_match:
-			[out, totals] = process_simple(line, out, totals, capacitors_re, capacitor_value_re, 'CAPACITOR|')
-		else:
-			out += f"\n{line} (unprocessed)"
-	return [out,totals]
 
-def print_part_counts(totals):
-	list = [f"{key}|{value}" for key, value in totals.items()]
-	list.sort()
-	for item in list:
-		print(item,)
+def process_simple(line, out, totals, line_regex, value_regex, value_prefix, separator):
+    line_regex_result = line_regex.match(line)
 
-def main():
-	out = ''
-	totals = {}
-	pedals = []
-	f = open(sys.argv[1], "r")
-	str_in = f.read()
-	[out,totals] = process(str_in, out, totals, pedals)
-	print(out)
-	print("TOTALS",)
-	print_part_counts(totals)
-	print("PEDALS")
-	for pedal in pedals:
-		print(pedal,)
+    out += f"\n{separator.join([group for group in line_regex_result.groups() if group != ''])}"
+    value_regex_result = (
+        f"{value_prefix}{separator}{value_regex.match(line).groups()[0]}"
+    )
+    if value_regex_result in totals:
+        totals[value_regex_result] += 1
+    else:
+        totals[value_regex_result] = 1
+    return [out, totals]
+
+
+def process(str_in, out, totals, projects, separator):
+    lines = str_in.split("\n")
+    for line in lines:
+        project_match = project_re.match(line)
+        if project_match:
+            out = process_project_name(line, out, "PROJECT", separator)
+            projects.append(
+                f"{project_match.groups()[0]}{separator}{project_match.groups()[1]}"
+            )
+            continue
+        part_type_match = part_type_re.match(line)
+        if part_type_match:
+            out = process_part_type(line, out, "PART CLASS", separator)
+            continue
+        resistor_match = resistors_re.match(line)
+        capacitor_match = capacitors_re.match(line)
+        ic_match = ics_re.match(line)
+        diodes_match = diodes_re.match(line)
+        transistors_match = transistors_re.match(line)
+        pots_match = pots_re.match(line)
+        switches_match = switches_re.match(line)
+
+        if ic_match:
+            [out, totals] = process_simple(
+                line, out, totals, ics_re, ic_value_re, "IC", separator
+            )
+        elif diodes_match:
+            [out, totals] = process_simple(
+                line, out, totals, diodes_re, diode_value_re, "DIODE", separator
+            )
+        elif pots_match:
+            [out, totals] = process_simple(
+                line, out, totals, pots_re, pot_value_re, "POT", separator
+            )
+        elif transistors_match:
+            [out, totals] = process_simple(
+                line,
+                out,
+                totals,
+                transistors_re,
+                transistor_value_re,
+                "TRANSISTOR",
+                separator,
+            )
+        elif switches_match:
+            [out, totals] = process_simple(
+                line, out, totals, switches_re, switch_value_re, "SWITCH", separator
+            )
+        elif resistor_match:
+            [out, totals] = process_simple(
+                line,
+                out,
+                totals,
+                resistors_re,
+                resistor_value_re,
+                "RESISTOR",
+                separator,
+            )
+        elif capacitor_match:
+            [out, totals] = process_simple(
+                line,
+                out,
+                totals,
+                capacitors_re,
+                capacitor_value_re,
+                "CAPACITOR",
+                separator,
+            )
+        else:
+            out += f"\n{line} (unprocessed)"
+            totals["UNPROCESSED"] += 1
+    return out
+
+
+def output_parts(parts, filename):
+    print(
+        "====PARTS====",
+    )
+    print(parts)
+    f = open(filename, "w")
+    f.write(parts)
+    f.close()
+
+
+def output_totals(totals, separator, filename):
+    list = [f"{key}{separator}{value}" for key, value in totals.items()]
+    list.sort()
+    out = ""
+    for item in list:
+        out += item + "\n"
+    print(
+        "====TOTALS====",
+    )
+    print(out)
+    f = open(filename, "w")
+    f.write(out)
+    f.close()
+
+
+def output_projects(projects, filename):
+    out = ""
+    print(
+        "====PROJECTS====",
+    )
+    for project in projects:
+        out += project + "\n"
+    print(out)
+    f = open(filename, "w")
+    f.write(out)
+    f.close()
+
+
+def main(in_file, parts_file, totals_file, projects_file, separator):
+    parts = ""
+    totals = {"UNPROCESSED": 0}
+    projects = []
+    f = open(in_file, "r")
+    str_in = f.read()
+    parts = process(str_in, parts, totals, projects, separator)
+    output_parts(parts, parts_file)
+    output_totals(totals, separator, totals_file)
+    output_projects(projects, projects_file)
+
 
 if __name__ == "__main__":
-	main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--in_file", help="The file to process")
+    parser.add_argument(
+        "--parts_file",
+        help="Filename to outputs a list of parts that can be ingested into a spreadsheet",
+        default="parts.txt",
+    )
+    parser.add_argument(
+        "--totals_file",
+        help="Filename to output a tabulated list of totals",
+        default="totals.txt",
+    )
+    parser.add_argument(
+        "--projects_file",
+        help="Filename to output a tabulated list of projects",
+        default="projects.txt",
+    )
+    parser.add_argument(
+        "--separator",
+        help="Character to use to separate columns in output",
+        default="|",
+    )
+    args = parser.parse_args()
+    main(
+        args.in_file,
+        args.parts_file,
+        args.totals_file,
+        args.projects_file,
+        args.separator,
+    )
